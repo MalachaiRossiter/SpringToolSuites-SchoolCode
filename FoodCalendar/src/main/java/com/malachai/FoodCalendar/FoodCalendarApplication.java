4679ab525adfc0d3630b7fe2378a2c4f51fd9868
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-11-09 23:56:08 -0500

    First Commit

---
+++ FoodCalendar/src/main/java/com/malachai/FoodCalendar/FoodCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.malachai.FoodCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCalendarApplication.class, args);
+	}
+
+}
---
+++ FoodCalendar/src/test/java/com/malachai/FoodCalendar/FoodCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malachai.FoodCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
